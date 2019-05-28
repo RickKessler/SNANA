2a18818205e8d6407119c8660f64366c53209eaf
Richard Kessler <kessler@kicp.uchicago.edu>
2019-05-28 10:29:59 -0500

    apply CUTWIN_TOBS to Bazin-fit epochs

--- src/sntools.h
+++ src/sntools.h
@@ -39,7 +39,7 @@
 
 #include "sndata.h"
 
-#define  SNANA_VERSION_CURRENT  "v10_73"                                             
+#define  SNANA_VERSION_CURRENT  "v10_73b"                                              
 
 #define LIGHT_km  2.99792458e5      // speed of light (km/s) 
 #define LIGHT_A   2.99792458e18     // speed of light (A/s) 

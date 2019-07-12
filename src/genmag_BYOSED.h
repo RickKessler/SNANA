0c2e646ed9c4c862bbd6d84cd6eb6b38e976ed24
David Jones <djones1741@midway2-login1.rcc.local>
2019-07-12 15:21:13 -0500

    merge conflict fixed

--- src/genmag_BYOSED.h
+++ src/genmag_BYOSED.h
@@ -1,12 +1,7 @@
 // Created Sep 2018
 
 // define pre-processor command to use python interface
-<<<<<<< HEAD
-#define USE_PYTHON  
-
-=======
 #define USE_PYTHON   
->>>>>>> master
 
 // ===========================================
 // global variables

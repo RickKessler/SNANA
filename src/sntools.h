e08150cc84d658ba6e12b7df96950fcfe14a66b2
Richard Kessler <kessler@kicp.uchicago.edu>
2020-06-05 17:42:43 -0500

    exclude FLAMERR outliers for writing TAKE_SPECTRUM SNR in SIMLIB

--- src/sntools.h
+++ src/sntools.h
@@ -42,7 +42,7 @@
 #include <stdbool.h>
 
 #include "sndata.h"
-#define  SNANA_VERSION_CURRENT  "v10_77c"                           
+#define  SNANA_VERSION_CURRENT  "v10_77d"                            
 
 // default cosmo params from Planck 2018 (https://arxiv.org/abs/1807.06209)
 #define OMEGA_MATTER_DEFAULT   0.315 

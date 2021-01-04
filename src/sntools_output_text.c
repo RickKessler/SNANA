ff24607c69dc54b09bc05f4fb793311e75a0b5d4
Richard Kessler <kessler@kicp.uchicago.edu>
2021-01-04 14:13:28 -0600

    increase MXCHAR_LINE to 3200

--- src/sntools_output_text.c
+++ src/sntools_output_text.c
@@ -57,7 +57,7 @@
 // Jun 03 2020: if MARZ option is set, remove [z=xxx] from CCID
 //              in TEXT SPECPLOT table.
 //
-// Jan 4 2021: MXCHAR_LINE -> 3000 (was 2500)
+// Jan 4 2021: MXCHAR_LINE -> 3200 (was 2500)
 // **********************************************
 
 char FILEPREFIX_TEXT[100];
@@ -65,7 +65,7 @@ char FILEPREFIX_TEXT[100];
 #define MXTABLE_TEXT 10 
 #define MXVAR_TEXT   MXVAR_TABLE
 #define MXEPVAR_TEXT 50   // for light curve epoch
-#define MXCHAR_LINE  3000 
+#define MXCHAR_LINE  3200 
 
 #define OPT_FORMAT_KEY   1
 #define OPT_FORMAT_CSV   2

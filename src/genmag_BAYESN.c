afa906a165b06679e2cce1d745961848b311fd4b
stevet40 <stephen.thorp@hotmail.co.uk>
2022-10-12 12:37:29 -0700

    Set up genmag_bayesn functions

--- src/genmag_BAYESN.c
+++ src/genmag_BAYESN.c
@@ -35,5 +35,32 @@ int init_genmag_bayesn__( char *version, int *optmask) {
 
 int init_genmag_bayesn(char *version, int optmask){
     printf("Hello from fortran hell");
+	return 0;
+}
 
+void genmag_BAYESN(
+		  int OPTMASK     // (I) bit-mask of options (LSB=0)
+		  ,int ifilt_obs  // (I) absolute filter index
+		  ,double *parList_SN   // DLMAG, THETA, AV, RV
+		  ,double mwebv   // (I) Galactic extinction: E(B-V)
+		  ,double z       // (I) Supernova redshift
+		  ,int    Nobs         // (I) number of epochs
+		  ,double *Tobs_list   // (I) list of Tobs (w.r.t peakMJD) 
+		  ,double *magobs_list  // (O) observed mag values
+		  ,double *magerr_list  // (O) model mag errors
+		  ) {
+	char fnam[] = "genmag_BAYESN";
+
+
+	//BEGIN
+	return;
+} //End of genmag_BAYESN
+ 
+void genmag_bayesn__(int *OPTMASK, int *ifilt_obs, double *parlist_SN,
+	       	double *mwebv, double *z, int *Nobs,
+	       	double *Tobs_list, double *magobs_list,
+	       	double *magerr_list) {
+	genmag_BAYESN(*OPTMASK, *ifilt_obs, parlist_SN, *mwebv, *z,
+		       	*Nobs, Tobs_list, magobs_list, magerr_list);
+	return;
 }
--- src/genmag_BAYESN.h
+++ src/genmag_BAYESN.h
@@ -1,3 +1,20 @@
 
 int init_genmag_bayesn(char *version, int optmask);
 int init_genmag_bayesn__( char *version, int *optmask);
+
+void genmag_BAYESN(
+		  int OPTMASK     // (I) bit-mask of options (LSB=0)
+		  ,int ifilt_obs  // (I) absolute filter index
+		  ,double *parList_SN   // DLMAG, THETA, AV, RV
+		  ,double mwebv   // (I) Galactic extinction: E(B-V)
+		  ,double z       // (I) Supernova redshift
+		  ,int    Nobs         // (I) number of epochs
+		  ,double *Tobs_list   // (I) list of Tobs (w.r.t peakMJD)
+		  ,double *magobs_list  // (O) observed mag values
+		  ,double *magerr_list  // (O) model mag errors
+        );
+
+void genmag_bayesn__(int *OPTMASK, int *ifilt_obs, double *parlist_SN,
+	       	double *mwebv, double *z, int *Nobs,
+	       	double *Tobs_list, double *magobs_list,
+	       	double *magerr_list);

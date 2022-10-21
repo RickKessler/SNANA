edac48a061d5f31d8897fcb4b505fae0d4e021d0
Richard Kessler <kessler@kicp.uchicago.edu>
2022-10-21 09:49:11 -0500

    handle updated data files with NOBS=0

--- util/update_data_files.py
+++ util/update_data_files.py
@@ -17,6 +17,7 @@ SNDATA_ROOT        = os.environ['SNDATA_ROOT']
 DATAKEY_VPEC       = "VPEC:"
 DATAKEY_FILTERS    = "FILTERS:"
 DATAKEY_VARLIST    = "VARLIST:"
+DATAKEY_NOBS       = "NOBS:"
 DATAKEY_OBS        = "OBS:"
 
 # =====================================
@@ -85,17 +86,27 @@ def get_list_data(path_data):
     return list_data_files
     # end get_list_data
     
-def update_list_file(path_data , rename_list):
+def update_list_file(version, rename_list, list_data_files_remove):
 
     # use linux sed to modify file names in [VERSION].LIST file
+    # inputs:
+    #  + version      data version to update [VERSION].LIST file
+    #  + rename_list  replace rename_list[0] with rename_list[1]
+    #                  in each data file name
+    #  + list_data_files_remove : 
+    #       list original file names to remove from [VERSION].LIST
 
-    version   = os.path.basename(path_data)
-    LIST_FILE = (f"{path_data}/{version}.LIST")
+    LIST_FILE = (f"{version}/{version}.LIST")
 
     substr_orig = rename_list[0]
     subtrs_new  = rename_list[1]
 
-    cmd = f"sed -i 's//{substr_orig}//{subtrs_new}//g' {LIST_FILE}"
+    if len(list_data_files_remove) > 0:
+        for data_file in list_data_files_remove:
+            cmd = f"sed -i '/{data_file}/d' {LIST_FILE}"
+            os.system(cmd)
+
+    cmd = f"sed -i 's/{substr_orig}/{subtrs_new}/g' {LIST_FILE}"
     os.system(cmd)
 
     return
@@ -164,7 +175,7 @@ def update_data_file(data_file, args):
 
     # Update data_file in outdir
     outdir          = args.version
-    path_data       = args.path_data
+    path_data       = args.path_data  # path to original data
     filter_dict     = args.filter_dict
     filters_remove  = args.filters_remove
     DO_MINUS_VPEC   = args.MINUS_VPEC
@@ -179,7 +190,9 @@ def update_data_file(data_file, args):
 
     DATA_FILE_OUT  = (f"{version}/{data_file}.gz")
 
-    nline = 0
+    nline_new  = 0
+    iline_NOBS = -9
+    NOBS_ORIG = 0; NOBS_NEW = 0
 
     contents     = f_orig.readlines()
     f_orig.close()
@@ -190,17 +203,18 @@ def update_data_file(data_file, args):
         iwd_band     = -1
 
     for line_orig in contents :
-        nline += 1
         line   = line_orig.decode('utf-8')
         wdlist = line.split()
 
         if len(wdlist) < 2:
             contents_new.append(line)
+            nline_new += 1
             continue
 
         ISKEY_FILTERS = (wdlist[0] == DATAKEY_FILTERS)
         ISKEY_VARLIST = (wdlist[0] == DATAKEY_VARLIST)
         ISKEY_OBS     = (wdlist[0] == DATAKEY_OBS )
+        ISKEY_NOBS    = (wdlist[0] == DATAKEY_NOBS )
 
         if ISKEY_FILTERS and filters_remove :
             f_remove_list = list(filters_remove)            
@@ -213,12 +227,16 @@ def update_data_file(data_file, args):
 
         if ISKEY_OBS:
             band_orig = wdlist[iwd_band]
+            NOBS_ORIG += 1
         else:
             band_orig = "?"
 
         if filters_remove:
             if band_orig[-1] in filters_remove:
                 continue
+
+        if ISKEY_OBS: 
+            NOBS_NEW += 1
 
         if DO_MINUS_VPEC and DATAKEY_VPEC in wdlist :
             j           = wdlist.index(DATAKEY_VPEC)
@@ -236,8 +254,30 @@ def update_data_file(data_file, args):
             line   = "  ".join(wdlist) + '\n'
 
         contents_new.append(line)
+        if ISKEY_NOBS:   iline_NOBS = nline_new
+        nline_new += 1
 
     # - - - - - - - - - - - 
+    # if NOBS has changed (e.g., remove filter bands), 
+    # update line with NOBS key
+    #print(f"xxx  NOBS = {NOBS_ORIG} -> {NOBS_NEW}  iline_OBS={iline_NOBS}")
+    if NOBS_NEW < NOBS_ORIG:
+        line   = contents_new[iline_NOBS]
+        wdlist = line.split()
+        key    = wdlist[0]
+        if key != DATAKEY_NOBS:
+            sys.exit(f"\n ERROR: key={key} but expect {DATAKEY_NOBS}\n" \
+                     f"line = {line}")
+        line = line.replace(str(NOBS_ORIG),str(NOBS_NEW))
+        contents_new[iline_NOBS] = line
+
+    # - - - - - - 
+    # do NOT update data file if no bands pass filter cut
+    if NOBS_NEW == 0:
+        print(f"  WARNING: no output obs in {data_file} " \
+              f"-> do not write data file")
+        return False
+        
 
     if args.rename is None:
         out_file = DATA_FILE_OUT
@@ -250,7 +290,8 @@ def update_data_file(data_file, args):
     with gzip.open(out_file, "wt") as o :
         o.write("".join(contents_new))
 
-    return
+    return True
+
     # end update_data_file
 
 def print_summary(args):
@@ -290,13 +331,17 @@ if __name__ == "__main__":
 
     nupd = 0
     n_data_file = len(list_data_files)
+    list_data_files_remove = []
     for data_file in list_data_files :
-        update_data_file(data_file, args)
-        nupd += 1
+        is_write = update_data_file(data_file, args)
+        if is_write:
+            nupd += 1
+        else:
+            list_data_files_remove.append(data_file)
 
     # if file names are modified, modify names in [VERSION].LIST file
     if args.rename:
-        update_list_file(path_data,args.rename_list)
+        update_list_file(version,args.rename_list,list_data_files_remove)
 
     print(f"\n Done modifying {nupd} of {n_data_file} " \
           f"data files under {version}\n")
@@ -304,5 +349,6 @@ if __name__ == "__main__":
     # - - - - - - 
     print_summary(args)
 
-    # END
+    # END main
+
 

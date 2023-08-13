938601539bb94389b911ce678ac9e55f9e1f6905
Richard Kessler <kessler@kicp.uchicago.edu>
2023-08-12 22:17:15 -0500

    MXROW_HOSTLIB -> 40M (was 10M)

--- src/sntools_host.h
+++ src/sntools_host.h
@@ -25,7 +25,8 @@
  May 5 2022: MXCHAR_LINE_HOSTLIB->900
 
  Feb 21 2023: increase ZMAX_HOSTLIB to be same as ZMAX_SNANA
-
+ 
+ Aug 11 2023: MXROW_HOSTLIB -> 40M (was 10M)
 
 ==================================================== */
 
@@ -55,7 +56,7 @@
 #define MXVAR_HOSTLIB       400  // max number of variables (NVAR:) in HOSTLIB
 #define MXVAR_WGTMAP_HOSTLIB 10  // max no. weight-map variables
 #define MXROW_WGTMAP      25000000  // 20 million, Alex Gagliano 09/2021
-#define MXROW_HOSTLIB     10000000  // 10 million, Alex Gagliano 09/2021
+#define MXROW_HOSTLIB     40000000  // max number or rows in HOSTLIB
 #define MXCHECK_WGTMAP     1000  // max no. galaxies to check wgt map
 #define MALLOCSIZE_HOSTLIB 40000 // incremental size of internal HOSTLIB array
 #define MXCOMMENT_HOSTLIB  40    // max number of lines for README file

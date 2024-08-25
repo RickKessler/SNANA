e3da7a77c6193c9eeb3cb6fbabda5b4eba5c7248
Richard Kessler <kessler@kicp.uchicago.edu>
2024-08-25 13:34:46 -0500

    bleh

--- util/plot_table.py
+++ util/plot_table.py
@@ -1531,7 +1531,7 @@ def plotter_func_driver(args, plot_info):
             hist2d_args = plot_info.hist2d_args
             counts, xedges, yedges, im = \
                 plt.hist2d(df.x_plot_val, df.y_plot_val, label=plt_legend,
-                           cmin=.1, alpha=plt_alpha, # cmap='Greys',
+                           cmin=.1, alpha=plt_alpha, cmap='Greys',
                            bins  = hist2d_args.bins,
                            range = hist2d_args.range,
                            norm  = hist2d_args.norm )
